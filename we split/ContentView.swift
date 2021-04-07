15a90543c261882efda8342254b8d9936cbe76d5
sebmaz <titan9389@me.com>
2021-04-07 23:20:00 +0400

    init

---
+++ we split/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  we split
+//
+//  Created by Sebouh Mazloumian on 06.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ we split/we_splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  we_splitApp.swift
+//  we split
+//
+//  Created by Sebouh Mazloumian on 06.04.21.
+//
+
+import SwiftUI
+
+@main
+struct we_splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
