03c19f54f00142d8cc02dd2c265c8a0eaa424dc4
sebmaz <titan9389@me.com>
2021-04-14 01:29:14 +0400

    finished app

--- we split/ContentView.swift
+++ we split/ContentView.swift
@@ -8,19 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+
+    let tips = [5, 10, 15, 20, 0]
+
+    var total: (perPerson: Float, grandTotal: Float) {
+        let peopleCount = Float(numberOfPeople) ?? 0
+        let tipSelection = Float(tips[tipPercentage])
+        let orderAmount = Float(checkAmount) ?? 0
+
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        // to not have NaN when dividing on 0
+        var perPerson : Float
+        if peopleCount <= 0 {
+            perPerson = grandTotal / (peopleCount + 1)
+        } else {
+            perPerson = grandTotal / peopleCount
+        }
+        
+        return (perPerson, grandTotal)
+    }
+
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("check amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+
+                    TextField("number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+                }
+
+                Section(header: Text("how much tip do you want to leave ?")) {
+                    Picker("tip percentage", selection: $tipPercentage
+                    ) {
+                        ForEach(0 ..< tips.count) {
+                            Text("\(self.tips[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section(header: Text("per person")) {
+                    Text("$\(total.perPerson, specifier: "%.2f")")
+                }
+
+                Section(header: Text("total with tips")) {
+                    Text("$\(total.grandTotal, specifier: "%.2f")")
+                }
             }
-            
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
+            .navigationTitle("WeSplit")
         }
     }
 }
